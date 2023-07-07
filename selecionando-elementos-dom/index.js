8f44f25fa5eb95384ae1bafd4b0d8c61e1096ef1
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-07 20:35:40 -0300

    Upando arquivos
    Selecionando elementos html no JS

---
+++ selecionando-elementos-dom/index.js
@@ -0,0 +1,13 @@
+//Selecionando elementos HTML com DOM
+
+//Selecionando o elemento pelo id:
+console.log(document.getElementById('nomeUsuario'))
+
+//Selacionando o elemento pelo nome da tag:
+console.log(document.getElementsByTagName('input'))
+
+//Selecionando o elemento pela classe:
+console.log(document.getElementsByClassName('campoTexto'))
+
+//Selecionando o elemento pelo name:
+console.log(document.getElementsByName('nome'))
