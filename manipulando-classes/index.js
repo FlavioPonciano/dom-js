a99d9ba634934c791c1bcc44a0bb1c2fa6338a98
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-13 11:13:12 -0300

    Upando arquivos
    Alterando o class name de um elemento html

---
+++ manipulando-classes/index.js
@@ -0,0 +1,3 @@
+function modificaClasse() {
+    document.getElementById('mensagem').className = 'estilo2'//alterando a classe atribuindo uma classe nova, no html gerando dois estilos com classes diferentes e atribuindo uma classe dessas para a div recebendo as alterções css
+}
